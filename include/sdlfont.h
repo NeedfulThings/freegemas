8a7f04b124c97eb5e6e0f0b0b4fa44449a51fddd
Julian Raschke <julian@raschke.de>
2011-11-16 00:14:02 +0100

    Reduced CustomFont/sdlfont to Gosu::Font wrapper
    Signed-off-by: Julian Raschke <julian@raschke.de>

--- include/sdlfont.h
+++ include/sdlfont.h
@@ -34,8 +34,6 @@
 #include <boost/shared_ptr.hpp>
 
 #include <boost/lexical_cast.hpp>
-#include <boost/unordered/unordered_map.hpp>
-#include <boost/foreach.hpp>
 #include <boost/tokenizer.hpp>
 
 #include <map>
@@ -45,135 +43,15 @@
 #include <iostream>
 using namespace std;
 
-#ifdef WIN32
-#include <SDL_ttf.h>
-#else
-#include <SDL/SDL_ttf.h>
-#endif
-
-class CustomFont : boost::noncopyable
+class CustomFont : public Gosu::Font
 {
-	TTF_Font* font;
-
-
-	class SDLSurface : boost::noncopyable
-	{
-		SDL_Surface* surface;
-
-	public:
-		SDLSurface(TTF_Font* font, const std::wstring& text, Gosu::Color c, int flags = 0)
-		{
-
-			SDL_Color color = { c.red(), c.green(), c.blue() };
-			surface = TTF_RenderUTF8_Blended(font, Gosu::wstringToUTF8(text).c_str(), color);
-
-			if (!surface)
-				throw std::runtime_error("NO PUEDORR >" 
-				+ Gosu::wstringToUTF8(text) + "<");
-		}
-
-		~SDLSurface()
-		{
-			SDL_FreeSurface(surface);
-		}
-
-		unsigned height() const
-		{
-			return surface->h;
-		}
-
-		unsigned width() const
-		{
-			return surface->w;
-		}
-
-		const void* data() const
-		{
-			return surface->pixels;
-		}
-	};
-
-	//*/
-
-	Gosu::Graphics & graphics;
-	boost::unordered_map<wchar_t, boost::shared_ptr<Gosu::Image> > fontGlyphs;
-
-	boost::shared_ptr<Gosu::Image> getGlyph(const wchar_t & w){
-		if(fontGlyphs.find(w) == fontGlyphs.end()){
-
-			const std::wstring text(1, w);
-
-			SDLSurface S(font, text, 0xffffffff);
-
-			Gosu::Bitmap B;
-			B.resize(S.width(), S.height());
-			std::memcpy(B.data(), S.data(), S.width() * S.height() * 4);
-
-			fontGlyphs[w].reset(new Gosu::Image(graphics, B));      
-		}
-
-		return fontGlyphs[w];    
-	}
-
-	int process(bool toDraw, const std::wstring & text, int x, int y, Gosu::ZPos z, 
-		int fx = 1, int fy = 1, Gosu:: Color color = 0xffffffff){
-
-			if(text == L"") return 0;
-
-			int acumX = 0;
-
-			BOOST_FOREACH(const wchar_t & w, text){
-				boost::shared_ptr<Gosu::Image> currentGlyph = getGlyph(w);
-				if(toDraw) currentGlyph-> draw(x + acumX, y, z, 0.5, 0.5, color);
-
-				acumX += currentGlyph -> width() / 2;
-			}
-
-			return acumX;
-	}
-
 public:
 	CustomFont(Gosu::Graphics & G, const std::wstring& fontName, unsigned fontHeight, int flags = 0)
-		: graphics(G)
+		: Font(G, L"./" + fontName, fontHeight, flags)
 	{
-		static int initResult = TTF_Init();
-		if (initResult < 0)
-			throw std::runtime_error("Could not initialize SDL_TTF");
-
-		// Try to open the font at the given path
-		font = TTF_OpenFont(Gosu::wstringToUTF8(fontName).c_str(), fontHeight*2);
-
-		if (!font)
-			throw std::runtime_error("Could not open TTF file " 
-			+ Gosu::wstringToUTF8(fontName));
-
-		/*
-		cout << "FHeight: " << TTF_FontHeight(font) << endl
-		<< "FAscent: " << TTF_FontAscent(font) << endl
-		<< "FDescen: " << TTF_FontDescent(font) << endl
-		<< "FSkip:   " << TTF_FontLineSkip(font) << endl; 
-		//*/
-
 	}
-
-	~CustomFont()
-	{
-		TTF_CloseFont(font);
-	}
-
-	unsigned textWidth(const std::wstring& text){
-		return process(false, text, 0, 0, 0, 0, 0, 0xffffffff);
-	}
-
 	int fontLineSkip(){
-		if(font)
-			return TTF_FontLineSkip(font);
-		return 0;
-	}
-
-	void draw(const std::wstring & text, int x, int y, Gosu::ZPos z, 
-		float fx = 1, float fy = 1, Gosu:: Color color = 0xffffffff){
-			process(true, text, x, y, z, fx, fy, color);
+		return height();
 	}
 };
 
